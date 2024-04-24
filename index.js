3845ae2d73c5813d1162080713f7da19ef467dbf
Sufiyan <sufiyan95@hotmail.com>
2024-04-24 22:14:25 +0800

    Added serverless.yml and index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.post = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "This is a post function!",
+      },
+      null,
+      2
+    ),
+  };
+};
